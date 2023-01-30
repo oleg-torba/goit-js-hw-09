d3682f3e041b282bbd6a39e5b4b0f3d20a5633e7
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2023-01-30 23:11:37 +0200

    dsds

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,30 @@
+const backColorTag = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', startSwitchColor);
+stopBtn.addEventListener('click', stopSwitchColor);
+
+let intervalId = null;
+
+function startSwitchColor() {
+  startBtn.setAttribute('disabled', '');
+
+  intervalId = setInterval(changeColor, 1000);
+
+  getRandomHexColor();
+}
+
+function stopSwitchColor() {
+  startBtn.removeAttribute('disabled', '');
+  clearInterval(intervalId);
+}
+
+function changeColor() {
+  backColorTag.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
