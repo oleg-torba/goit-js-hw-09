2714dd769fe46db83fa050fb35c0c5c0af7a1969
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2023-02-04 23:16:45 +0200

    some

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,44 +1,90 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-const dateInput = document.querySelector("#datetime-picker")
-const days = document.querySelector("[data-days]");
-const hours = document.querySelector("[data-hours]");
-const minutes = document.querySelector("[data-minutes]");
-const seconds = document.querySelector("[data-seconds]");
-const startBtn = document.querySelector("[data-start]");
-startBtn.addEventListener("click", startTimer)
 
-function startTimer (){
-    const currentDate = new Date;
-    console.log(currentDate)
-}
+    const startBtn= document.querySelector('[data-start]');
+    const showDays= document.querySelector('[data-days]');
+    const showHours= document.querySelector('[data-hours]');
+   const showMinutes= document.querySelector('[data-minutes]');
+    const showSeconds= document.querySelector('[data-seconds]');
+
+
+startBtn.disabled = 'disabled';
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+        if (this.selectedDates[0] > Date.now()) {
+            startBtn.disabled = null;
+        } else {
+           return alert('Please choose a date in the future');
+        }
+  },
+};
 
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
+const picker = flatpickr("#datetime-picker", options);
+
+
+
+class Timer {
+    constructor({onTick}) {
+        this.intervalId = null;
+        this.isActive = false;
+        this.onTick = onTick;
+    };
+    start() {
+        if (this.isActive) {
+            return;
+        }
+        const startTime = picker.selectedDates[0].getTime();
+        this.isActive = true;
+
+        this.intervalId = setInterval(() => {
+            const currentTime = Date.now();
+            const deltaTime = startTime - currentTime;
+
+            if (deltaTime < 0) {
+                clearInterval(this.intervalId);
+                return;
+            }
+            
+            const time = this.convertMs(deltaTime);
+            this.onTick(time);
+        }, 1000);
+        
+       startBtn.disabled = 'disabled';
+    };
+    convertMs(ms) {
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+        
+        const days = this.addLeadingZero(Math.floor(ms / day));
+        const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+        const minutes = this.addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+        const seconds = this.addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+        return { days, hours, minutes, seconds };
+    };
+    addLeadingZero(value) {
+        return String(value).padStart(2, '0');
+    };
+};
+
+const timer = new Timer({
+    onTick: changeFormat,
+});
+
+function changeFormat ({ days, hours, minutes, seconds }) {
+    showDays.textContent = `${days}`;
+    showHours.textContent = `${hours}`;
+    showMinutes.textContent = `${minutes}`;
+    showSeconds.textContent = `${seconds}`;
+};
+
+
+startBtn.addEventListener('click', timer.start.bind(timer));
