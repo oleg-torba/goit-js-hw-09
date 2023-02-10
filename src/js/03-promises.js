b66bdf1f1f171860546d77250ff805f57b85cc8f
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2023-02-10 21:49:04 +0200

    promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const form = document.querySelector('.form');
+form.addEventListener('submit', formSubmit);
+
+let position = 1;
+let step = 0;
+let delay = 0;
+let amount = 0;
+function formSubmit(event) {
+  event.preventDefault();
+  step = Number(form.elements.step.value);
+  delay = Number(form.elements.delay.value);
+  amount = Number(form.elements.amount.value);
+  startPromise();
+  event.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, delay);
+  });
+}
+
+function startPromise() {
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) =>
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      )
+      .catch(({ position, delay }) =>
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
+    position += 1;
+    delay += step;
   }
 }
+createPromise().then(startPromise());
+
+
